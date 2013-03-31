f2642a0db52e35e00ca3ede05489e4fabf2a056d
x <x@example.com>
2013-03-31 17:08:32 -0400

    added some dirs

--- aliases-public.sh
+++ aliases-public.sh
@@ -1,10 +1,10 @@
+alias no "pbcopy < ~/.no.txt"
 alias gcomp 'open https://mail.google.com/mail/u/0/#compose/\!*'
 alias mcomp "open 'https://mail.google.com/mail/u/0/x/?&v=b&eot=1&pv=tl&cs=b'"
 alias dv "setenv vstr ~/Desktop/cwnote_`date +20%yy%mm%dd%Hh%M`;grep '[A-z]' $gtddir/cwttd_20* | sort -rn | cut -d_ -f2- | sed 's/\:/\: /' | pbcopy; vv"
 alias dv "setv;grep '[A-z]' $gtddir/cwttd_20* | sort -rn | cut -d_ -f2- | sed 's/\:/\: /' | pbcopy; vv"
 alias learn "source $cwaliases ; source $cwpaliases"
-alias teach vi + $cwaliases
-alias pteach vi + $cwpaliases
+alias teach "vi + $cwaliases;vi + $cwpaliases"
 alias l '/bin/ls -ltrFsA'
 alias mi 'mv -i'
 alias up "cd .."
