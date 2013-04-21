d8e44cece44e4a78506d66d56d500c5beb923ff3
chrishwiggins <chris@hackny.org>
2013-04-21 09:26:14 -0400

    yak shaving

--- aliases-public.sh
+++ aliases-public.sh
@@ -1,34 +1,37 @@
-alias no "pbcopy < ~/.no.txt"
-alias gcomp 'open https://mail.google.com/mail/u/0/#compose/\!*'
-alias mcomp "open 'https://mail.google.com/mail/u/0/x/?&v=b&eot=1&pv=tl&cs=b'"
-alias dv "setenv vstr ~/Desktop/cwnote_`date +20%yy%mm%dd%Hh%M`;grep '[A-z]' $gtddir/cwttd_20* | sort -rn | cut -d_ -f2- | sed 's/\:/\: /' | pbcopy; vv"
-alias dv "setv;grep '[A-z]' $gtddir/cwttd_20* | sort -rn | cut -d_ -f2- | sed 's/\:/\: /' | pbcopy; vv"
+
+
+# unix essentials
 alias l '/bin/ls -ltrFsA'
 alias mi 'mv -i'
 alias up "cd .."
+alias please sudo
+
+# places to go
 alias cd-htm cd /Users/wiggins/Documents/public_html
 alias cd-mise cd $mise
+
 alias dump "learn;pbpaste > `datestr`"
-alias please sudo
 alias jsc /System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc
 alias dusort "learn;du | sort -nr >! dusort_`date +%yy%mm%dd_%Hh%Mm%Ss`"
 alias rstudio open /Applications/RStudio.app/
 alias permute "perl -MList::Util=shuffle -e 'print shuffle <>'"
 alias hai open http://example.com
 alias chrome "open ~/mise/bin/chrome"
-alias gread "fox  'http://www.google.com/reader/view/#overview-page'"
 alias fox "open -a 'Firefox' \!:*"
-alias mm "open https://mail.google.com/mail/u/0/x/"
 alias inpr "open http://www.npr.org/infiniteplayer/"
 
-# google stuff
+# google-fu
+alias gcomp 'open https://mail.google.com/mail/u/0/#compose/\!*'
+alias mcomp "open 'https://mail.google.com/mail/u/0/x/?&v=b&eot=1&pv=tl&cs=b'"
+alias mm "open https://mail.google.com/mail/u/0/x/"
+alias gread "fox  'http://www.google.com/reader/view/#overview-page'"
 #alias gcal fox http://www.google.com/calendar/render
 #alias gcal camino http://www.google.com/calendar/render
 #alias gcal camino https://www.google.com/calendar/
 #alias gcal fox https://www.google.com/calendar/
 #alias gcal chrome http://www.google.com/calendar/render
 alias gcal fox https://www.google.com/calendar/b/0/render
-# NB meant for google <- /usr/local/bin/google
+# NB: 'gc' intended for use with google <- /usr/local/bin/google
 alias gc google calendar add
 
 #news
@@ -50,12 +53,16 @@ alias aalias 'echo "alias \!*" >> $cwaliases ; learn '
 alias palias 'echo "alias \!*" >> $paliases ; learn '
 alias learn "source $cwaliases ; source $cwpaliases"
 alias teach "vi + $cwaliases;vi + $cwpaliases"
+alias gugc "git pull origin master;git commit -a;git push origin master"
 alias miseup "cd-mise;gugc"
 
 # misc:
 alias sniff open /Applications/iStumbler.app/
 alias estrip "pbpaste | fix | tr ' , :;[]=<>' '\n' | grep @ | tr '\n' ' ' | fix | sed -e 's/ [ ]*/,/g' | pbcopy"
 ## misc auxfile tricks:
+alias dv "setenv vstr ~/Desktop/cwnote_`date +20%yy%mm%dd%Hh%M`;grep '[A-z]' $gtddir/cwttd_20* | sort -rn | cut -d_ -f2- | sed 's/\:/\: /' | pbcopy; vv"
+alias dv "setv;grep '[A-z]' $gtddir/cwttd_20* | sort -rn | cut -d_ -f2- | sed 's/\:/\: /' | pbcopy; vv"
+alias no "pbcopy < ~/.no.txt"
 alias avail "vi ~/available.txt; pbcopy < ~/available.txt"
 ### taking/using quicknotes:
 alias v 'setenv vstr $ndir/cwnote_`date +20%yy%mm%dd%Hh%M`; vi $vstr; echo vstr=$vstr'
