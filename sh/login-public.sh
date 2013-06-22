c16cd7eed1afaac47474be07fc64e28e6df21b3a
chrishwiggins <chris@hackny.org>
2013-06-22 02:25:04 -0400

    ->_ (i.e., - > _  (i.e., no shift key spares me RSI))

---
+++ sh/login-public.sh
@@ -0,0 +1,36 @@
+umask 022
+
+# cw-specific aliases
+alias learn "source $cwaliases; source  ~/mise/aliases-public.sh"
+alias cset "set path = ( /usr/local/bin ~//Documents/Scripts/perl ~//Documents/Scripts/osa ~//Documents/Scripts/sh/csh ~//bin /opt/local/bin /usr/local/mysql/bin $path /opt/local/libexec/gnubin    /usr/local/sbin ~/mise/* )"
+
+# set some variables
+setenv EDITOR /usr/bin/vi
+setenv HOMEBREW_TEMP /usr/local/Cellar
+set savehist=1000
+set prompt="%n@%m{%c1}%\!: "
+
+# Don't overwrite existing files with the redirection character ">"
+set noclobber
+
+### TEXSTUFF
+# Add /usr/local/texlive/2011/texmf/doc/man to MANPATH, if not dynamically determined.
+# set manpath = ( $manpath /usr/local/texlive/2011/texmf/doc/man )
+set manpath = ( /usr/local/texlive/2011/texmf/doc/man )
+# Add /usr/local/texlive/2011/texmf/doc/info to INFOPATH.
+#set infopath = ( $infopath /usr/local/texlive/2011/texmf/doc/info )
+set infopath = ( /usr/local/texlive/2011/texmf/doc/info )
+#  Most importantly, add /usr/local/texlive/2011/bin/universal-darwin
+#  to your PATH for current and future sessions.
+set path = ( $path /usr/local/texlive/2011/bin/universal-darwin )
+# CWADD: add /usr/texbin
+set path = ( $path /usr/texbin )
+
+# CWADD: i like it here.
+set path = ( $path . )
+
+# actually do stuff
+## set path
+cset
+## learn my aliases
+learn
