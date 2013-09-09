cd26fbca7410211e6ba8b2be29b752f4bf9e8daf
x <x@example.com>
2013-09-09 17:42:34 -0400

    added elesevier alias to help say no AT SCALE to elesevier.

--- sh/aliases-public.sh
+++ sh/aliases-public.sh
@@ -121,3 +121,4 @@ alias get git clone
 
 #alias airport 'open /Applications/Utilities/AirPort\ Admin\ Utility.app/'
 alias airport 'open /Applications/Utilities/AirPort\ Utility.app/'
+alias ele "echo 'For reasons explained in detail at http://bactra.org/weblog/864.html, I will not review for any Elsevier publication. I look forward to assisting your journal in the future, after it switches publishers.' | pbcopy"
