d27d5770864dcb7a01796a7da91b2e5d7f295678
x <x@example.com>
2018-09-07 14:00:01 -0400

    moar aliases

--- sh/aliases-public.sh
+++ sh/aliases-public.sh
@@ -283,7 +283,7 @@ alias vi-null vim -u NONE
 alias nterm 'open `find ~/gd/aux/osx-terminal-themes/schemes | gshuf | head -\!:* | normalize`'
 # alias term 'open `find ~/gd/aux/osx-terminal-themes/schemes | gshuf | head -1 | normalize`'
 # good fu: exploits quick add but adds browser to edit event, send invitesjj
-alias oslow 'fox `slow \!:* | tr '"'"' '"'"' '"'"'\n'"'"' | grep ^http`'
+alias oslow 'pers-gmail-browser `slow \!:* | tr '"'"' '"'"' '"'"'\n'"'"' | grep ^http`'
 alias logo open /Applications/ACSLogo.app/
 alias remake 'vi makefile'
 alias polls "lynx -dump 'http://projects.fivethirtyeight.com/2016-election-forecast/?ex_cid=rrpromo#plus' | grep -A 2 'Hillary Clinton' | more | head -3 | grep '%'; lynx -dump 'http://www.nytimes.com/interactive/2016/upshot/presidential-polls-forecast.html' | more | grep Clinton | grep 'chance to win'"
