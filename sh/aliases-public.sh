e272b79890db43ce3be0e2a4acb5d37738d13228
x <x@example.com>
2020-01-23 04:00:01 -0500

    moar aliases

--- sh/aliases-public.sh
+++ sh/aliases-public.sh
@@ -427,3 +427,4 @@ alias gvs g +visual studio+ OR vscode
 alias nytrss "curl --silent https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml | grep -A 1 title"
 alias g0 py3 /usr/local/bin/googler
 alias g1 "open https://www.google.com/imghp?sout=1"
+alias linter pylint -d W0311 -d R0913 -d C0116 -d W0621 -d C0103
