8b0753836e7fcefa6a96ff46baca88e10945f6f4
x <x@example.com>
2023-08-19 17:26:39 -0400

    ocr latest screenshot

---
+++ bash/ocr_latest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the latest screenshot file
+latest_screenshot=$(ls -t ~/Shots/ | grep -E '^screenshot' | head -n 1)
+
+# Check if a screenshot is found
+if [ -z "$latest_screenshot" ]; then
+    echo "No screenshot found."
+    exit 1
+fi
+
+# Run Tesseract OCR on the latest screenshot
+tesseract ~/Shots/"$latest_screenshot" /tmp/ocr.txt
+
+# Inform the user
+echo "OCR output written to /tmp/ocr.txt"
