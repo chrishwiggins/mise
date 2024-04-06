6dc0b61cd13cbb7aa929e66f40cd175de4cc648d
wiggins <you@example.com>
2024-04-06 12:18:52 -0400

    mise

--- sh/aliases-public.sh
+++ sh/aliases-public.sh
@@ -440,7 +440,7 @@ alias nytrss "curl --silent https://rss.nytimes.com/services/xml/rss/nyt/HomePag
 alias g0 googler
 alias g1 "open https://www.google.com/imghp?sout=1"
 alias linter pylint -d W0311 -d R0913 -d C0116 -d W0621 -d C0103
-alias irl "echo 'Meeting in person preferred; if not convenient, we could meet via Google Hangouts; if also not convenient, we could talk by phone. Please do let me know!'|pbcopy"
+alias irl-text "echo 'Meeting in person preferred; if not convenient, we could meet via Google Hangouts; if also not convenient, we could talk by phone. Please do let me know!'|pbcopy"
 alias gift "pbcopy < $mise/aux/gift.txt"
 
 alias vin "vi -c 'set nonumber'"
