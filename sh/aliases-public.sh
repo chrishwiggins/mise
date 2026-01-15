b6f0667eba7b16c00b29eaf732d3b267559a28bb
wiggins <you@example.com>
2026-01-15 03:55:32 -0500

    token monitor

--- sh/aliases-public.sh
+++ sh/aliases-public.sh
@@ -520,6 +520,7 @@ alias newer 'find . -type f -newermt "\!:1"'
 alias yt-xcript yt-dlp --write-auto-sub --skip-download
 alias yt-mp3 'yt-dlp -x --audio-format mp3 -o "youtube_audio.%(ext)s"'
 alias yt-mp4 "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' "
+alias yt-video 'yt-dlp -o "%(title)s.%(ext)s"'
 #alias sniff "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s"
 alias plan "echo 'dream in years; plan in months; evaluate in weeks; ship daily'"
 alias nato "echo 'Alfa|Bravo|Charlie|Delta|Echo|Foxtrot|Golf|Hotel|India|Juliett|Kilo|Lima|Mike|November|Oscar|Papa|Quebec|Romeo|Sierra|Tango|Uniform|Victor|Whiskey|Xray|Yankee|Zulu'|tr '|' '\n'"
